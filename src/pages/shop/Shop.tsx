d632ee490c9dad476a08a5a0224f0ad1c27c8956
Kalel Poeta <kalel.poeta@orbisag.ch>
2023-02-01 11:06:25 +0100

    Page files and navbar

---
+++ src/pages/shop/Shop.tsx
@@ -0,0 +1,10 @@
+export function Shop() {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>Lelak Store</h1>
+      </div>
+      <div className="products"></div>
+    </div>
+  );
+}
